539b0418a7b4136d144640af99989dba55ded822
Aravind Kumar Buduri <aravind.buduri@gmail.com>
2022-10-23 14:06:51 +0000

    xdp: Support for kParser as bpf helper function
    	xdp program which is loaded into xdp hook calls
    	xdp helper function to get the metadata.
    
    	bpf helper function prototype for kParser.
    	long bpf_xdp_kparser(struct xdp_buff *xdp_md,
    			     void *conf, u32 conf_len,
    			     void *buf, u32 len);
                xdp_md          - xdp buffer pointer
                conf            - kParser parser identifier
    			      (hash key,name)
                conf_len        - size of conf in bytes
                buf             - metadata buffer to pass
    			      to kparser
                len             - metadata size in bytes
    
    	This helper parses a packet using the kParser.
    	The metadata produced by the parser is returned
    	in the buf pointer whose size is len. conf provides
    	a key that identifies the parser to invoke and
    	conf_len is the length of the key
    
    	metadata is mapped to bpf map.So that user space bpf
            program or bpftool can read the map(metadata is
            displayed in BTF format).
    
            funchooks are callbacks for packet parsing functions
    	of the kParser module and will be populated after
    	the kParser module is loaded.
    
            bpf xdp helper function is defined for kernel
    	parser(kParser).
            kParser is configured via userspace ip command.
            kParser data path API's as mentioned in kparser.h are
    	invoked using registered callback hooks.
            xdp frame is passed on to kParser via the xdp helper function
            and metadata is populated in the user specified buffer.
    
            xdp user space component, which loads xdp kernel component
            into xdp hook and displays number of packets
            transmitted/received per second.
    
    Signed-off-by: Aravind Kumar Buduri <aravind.buduri@gmail.com>

---
+++ samples/bpf/metadata_def.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause) */
+/* Copyright (c) 2022-23 Aravind Kumar Buduri <aravind.buduri@gmail.com>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+struct user_frame {
+	unsigned short ip_offset;
+	unsigned short l4_offset;
+	unsigned int ipv4_addrs[2];
+	unsigned short ports[2];
+} __packed;
+
+struct user_metadata {
+	struct user_frame frames;
+} __packed;
